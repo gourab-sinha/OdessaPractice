25e2fb1814d873c76daf46f671bb5a0efce9f267
gourab19964u <gourab19964u@gmail.com>
2018-10-04 10:32:51 +0530

    Csharp Hello world

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            int[] oldNUmbers = { 1, 2, 3, 4, 5 };
+            int number = oldNUmbers[2];
+            Console.WriteLine(number);
+            int i = 0;
+            for(;i < 5; i++)
+            {
+                Console.WriteLine(oldNUmbers[i]);
+            }
+            Console.ReadKey();
+            //Console.ReadKey();
+        }
+    }
+}
