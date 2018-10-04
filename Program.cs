0881276acd454cb53bb97868a22433799204d7ff
gourab19964u <gourab19964u@gmail.com>
2018-10-04 14:31:47 +0530

    Break_continue

--- Program.cs
+++ Program.cs
@@ -11,14 +11,32 @@ namespace Project11
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
-            int[] oldNUmbers = { 1, 2, 3, 4, 5 };
-            int number = oldNUmbers[2];
-            Console.WriteLine(number);
+            int[] oldNUmbers = { 1, 2, 3, 4, 5 }; // 1D Array, static declaration
+            int number = oldNUmbers[2]; // assign value of index 2 to number 
+            Console.WriteLine(number); // print number
             int i = 0;
-            for(;i < 5; i++)
+            /*while(i<5)
             {
                 Console.WriteLine(oldNUmbers[i]);
+                //Console.WriteLine("\n");
+                i += 1;
             }
+            i = 0;
+            do
+            {
+                Console.WriteLine(oldNUmbers[i]);
+                i += 1;
+            } while (oldNUmbers[i] > 4);*/
+            int counter = 0;
+            for (; i < 5; i++) // print all numbers
+            {
+                if (oldNUmbers[i] == 3)
+                {
+                    break;
+                }
+                counter += 1;
+            }
+            Console.WriteLine(counter);
             Console.ReadKey();
             //Console.ReadKey();
         }
