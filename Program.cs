9ff5cd49eadde8f2843d1851f8fa4002917c4bbd
gourab19964u <gourab19964u@gmail.com>
2018-10-05 15:50:11 +0530

    ArrayList with Employee objects

--- Program.cs
+++ Program.cs
@@ -6,33 +6,21 @@ using System.Threading.Tasks;
 
 namespace Project11
 {
-    class car
-    {
-        string _Brand;
-        string _Model;
-        int _speed;
-        double _EnginePower;
-        public void AcceptCarDetails()
-        {
-            _Brand = Console.ReadLine();
-            _Model = Console.ReadLine();
-            _speed = Convert.ToInt32(Console.ReadLine());
-            _EnginePower = Convert.ToDouble(Console.ReadLine());
-        }
-
-        public void DisplayCarDetails()
-        {
-            Console.Write($"Brand: {_Brand} \n Model: {_Model} \n Speed: {_speed} \n EnginePower: {_EnginePower} \n");
-            Console.ReadKey();
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            car newcar = new car();
-            newcar.AcceptCarDetails();
-            newcar.DisplayCarDetails();
+            int[] array = new int[10];
+            for(int i=0;i<10;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(var item in  array)
+            {
+                Console.Write(item);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
