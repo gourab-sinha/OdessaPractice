08ce4d42007e43118c9d556470fc80249b19f2b8
gourab19964u <gourab19964u@gmail.com>
2018-10-05 11:00:10 +0530

    Object Oriented Programming and Design

--- Program.cs
+++ Program.cs
@@ -6,39 +6,32 @@ using System.Threading.Tasks;
 
 namespace Project11
 {
+    class car
+    {
+        string _Brand;
+        string _Model;
+        int _speed;
+        double _EnginePower;
+        public void AcceptCarDetails(string Brand, string Model, int Speed, double EnginePower)
+        {
+            _Brand = Brand;
+            _Model = Model;
+            _speed = Speed;
+            _EnginePower = EnginePower;
+        }
+        public void DisplayCarDetails()
+        {
+            Console.Write($"Brand: {_Brand} \n Model: {_Model} \n Speed: {_speed} \n EnginePower: {_EnginePower} \n");
+            Console.ReadKey();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
-            int[] oldNUmbers = { 1, 2, 3, 4, 5 }; // 1D Array, static declaration
-            int number = oldNUmbers[2]; // assign value of index 2 to number 
-            Console.WriteLine(number); // print number
-            int i = 0;
-            /*while(i<5)
-            {
-                Console.WriteLine(oldNUmbers[i]);
-                //Console.WriteLine("\n");
-                i += 1;
-            }
-            i = 0;
-            do
-            {
-                Console.WriteLine(oldNUmbers[i]);
-                i += 1;
-            } while (oldNUmbers[i] > 4);*/
-            int counter = 0;
-            for (; i < 5; i++) // print all numbers
-            {
-                if (oldNUmbers[i] == 3)
-                {
-                    break;
-                }
-                counter += 1;
-            }
-            Console.WriteLine(counter);
-            Console.ReadKey();
-            //Console.ReadKey();
+            car newcar = new car();
+            newcar.AcceptCarDetails("Honda", "RXTX", 50, 700.00);
+            newcar.DisplayCarDetails();
         }
     }
 }
